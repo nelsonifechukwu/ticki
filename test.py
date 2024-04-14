56deab4b2ce67ad01eac15643d90d1fc12ed4833
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2024-04-15 00:23:30 +0100

    fixed found the best image comparison mix using deepFace

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ import os
 import argparse
 import pandas as pd
 import pickle
+import dlib
 from deepface import DeepFace
 from deepface.basemodels import VGGFace
 from retinaface import RetinaFace
 import matplotlib.pyplot as plt
 import face_recognition
+from PIL import Image 
 
 
 ## Get face, dfsd, face-recognition, deepface
@@ -20,7 +22,7 @@ class faceDetails:
         self.pic_name = pic_name
         faceDetails.img_deets[encoding] = pic_name
 
-location = "faces/Screenshot 2024-04-12 at 13.51.04.png"
+location = "faces/Screenshot 2024-04-13 at 23.47.03.png"
 def showwith_retina():
     faces = RetinaFace.detect_faces(img_path = location)
     r_img = cv.imread(location)
@@ -32,42 +34,32 @@ def showwith_retina():
     plt.imshow(r_img)
     plt.show()
 
-showwith_retina()
 
-#extract the faces
-def extract_faces():
+def find_face_encodings(image_path):
+    # reading image
+    image = cv.imread(image_path)
+    # get face encodings from the image
+    face_enc = face_recognition.face_encodings(image)
+    # return face encodings
+    return face_enc[0]
+
+
+def faces_to_embeddings():
+
     faces = RetinaFace.extract_faces(img_path=location, align=True)
+    for i, face in enumerate(faces):
+        for f in faces[i+1:]:
 
+            imga = Image.fromarray(face)
+            imga.save('my.png')
 
+            imgb = Image.fromarray(f)
+            imgb.save('myf.png')
+            result = DeepFace.verify(
+                "myf.png", "my.png", model_name="DeepID", normalization="ArcFace", distance_metric="euclidean_l2", detector_backend="mtcnn", enforce_detection=False)
+            print(result)
+            plt.imshow(face)
+            plt.imshow(f)
+            plt.show()
 
-# dfs = DeepFace.find(img_path="Screenshot 2024-04-12 at 15.50.37.png",
-#                     db_path="faces", model_name="Dlib")
-
-# print(dfs)
-# df0 = dfs[0]
-# df1=dfs[1]
-# df0=pd.concat([df0, df1])
-# # print(len(df0))
-# for df in range(len(df0)):
-#     img_path = df0.iloc[df]["identity"]
-#     img = cv.imread(img_path)
-#     pt1 = (df0.iloc[df]["target_x"],df0.iloc[df]["target_y"])
-#     pt2 = (pt1[0]+df0.iloc[df]["target_w"],pt1[1]+df0.iloc[df]["target_h"])
-#     cv.rectangle(img,pt1,pt2, color=(0,255,0), thickness=2 )
-#     img = img[:,:,::-1]
-#     plt.imshow(img)
-#     plt.show()
-
-#Extract all faces
-def extract_faces():
-    image = face_recognition.load_image_file(
-        location)
-    r_img = cv.imread(location)
-    face_locations = face_recognition.face_locations(
-        image, number_of_times_to_upsample=3, model="cnn")
-    for location in face_locations:
-        pt1 = (location[3], location[0])
-        pt2 = (location[1], location[2])
-        cv.rectangle(r_img, pt1, pt2, color = (0, 255, 0), thickness = 2)
-    plt.imshow(r_img)
-    plt.show()
+faces_to_embeddings()
