57e31faaecd3e92073fcaf29ab081ab8c99886de
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2024-04-29 17:24:05 +0100

    implemented extract all faces from a directory of images with name as metadata

---
+++ app/cbir.py
@@ -0,0 +1,110 @@
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.models import Model
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from deepface import DeepFace
+from deepface.basemodels import VGGFace
+from retinaface import RetinaFace
+import os
+from PIL import Image
+
+# See https://keras.io/api/applications/ for details
+
+class ImageProcessor:
+    
+    base_model = VGG16(weights='imagenet')
+    model = Model(inputs=base_model.input,
+                    outputs=base_model.get_layer('fc1').output)
+    @staticmethod    
+    def extract_faces(database: Path):
+        """
+        Extract faces from all images in a directory
+        Args:
+            database: path to the directory containing images
+        """
+        if not isinstance(database, Path):
+            raise TypeError("The 'database' parameter must be a Path object.")
+        if not database.exists():
+            raise FileNotFoundError(f"The directory '{database}' does not exist.")
+    
+        faces_directory = database / "faces"
+        faces_directory.mkdir(parents=True, exist_ok=True)
+                        
+        all_pics = Path(database).iterdir()
+        for pic in all_pics:
+            if pic.is_file():
+                print(pic)
+                faces = RetinaFace.extract_faces(img_path=str(pic), align=True, expand_face_area=20)
+                for i, face in enumerate(faces):
+                    if face.any():
+                        img = Image.fromarray(face)
+                        face_filename = f"{pic.stem}_{i}.png"
+                        face_filepath = faces_directory / face_filename
+                        img.save(face_filepath)
+    
+    @staticmethod          
+    def feature_extractor(faces: Path):
+        """
+        Extract features from a directory of faces
+        Args:
+            faces: path
+        """
+        path=faces
+        for face in path.iterdir():       
+             # Convert the image to a NumPy array
+            if face.suffix != ".npy":
+                img = Image.open(face)
+                img = img.resize((224, 224))  # VGG must take a 224x224 img as an input
+                img = img.convert('RGB')  # Make sure img is color
+                
+                # To np.array. Height x Width x Channel. dtype=float32
+                x = image.img_to_array(img)
+                
+                # (H, W, C)->(1, H, W, C), where the first elem is the number of img
+                x = np.expand_dims(x, axis=0)
+                x = preprocess_input(x)  # Subtracting avg values for each pixel
+                
+                # Get the feature vector
+                feature = ImageProcessor.model.predict(x)[0]  # (1, 4096) -> (4096, )
+                image_embedding = feature / np.linalg.norm(feature)  # Normalize
+
+                # Save the array to a .npy file
+                np.save(f"{path}/{face.stem}" + ".npy", image_embedding)
+
+
+
+process = ImageProcessor()
+# process.feature_extractor(faces=Path("app/static/database/nei"))
+process.extract_faces(database=Path.cwd() / "app" / "static" / "database")
+
+# def compare_pics(database:str, test_input:str):
+#     all_pics = os.listdir(database)
+#     for i, pic in enumerate(all_pics):
+#         location = Path.cwd() / f"{database}" / f"{pic}"
+#         faces = RetinaFace.extract_faces(img_path=location, align=True, expand_face_area=20)
+#         for  face in faces:
+#             if face.any():
+#                 img = Image.fromarray(face)
+#                 img.save('f.png')
+#                 result = DeepFace.verify('f.png',
+#                             test_input, model_name="ArcFace", normalization="ArcFace", distance_metric="euclidean_l2", detector_backend="mtcnn", enforce_detection=False)
+#                 print(test_input, location, result["verified"])
+#                 plt.imshow(face)
+#                 plt.show(block=False)
+#                 plt.pause(1)
+#                 plt.close()
+# # compare_pics('girl', 'girl/test.JPG')
+
+# def showwith_retina():
+#     location = "a2342e6d-a248-497a-ae02-5eacd85213be.JPG"
+#     faces = RetinaFace.detect_faces(img_path=location)
+#     r_img = cv.imread(location)
+#     for face in faces:
+#         place = faces[f"{face}"][f"facial_area"]
+#         pt1 = (place[0], place[1])
+#         pt2 = (place[2], place[3])
+#         cv.rectangle(r_img, pt1, pt2, color=(0, 255, 0), thickness=2)
+#     plt.imshow(r_img)
+#     plt.show()
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,77 @@
+#implement several features:
+"""
+save only faces in images
+select only similar images
+better ui
+track and run embeddings code when new image is added (like git) -> threads or check if no of new list > old list
+"""
 from app import app
+# Standard library imports
 
-@app.route('/',  methods=['GET'])
+import sys
+sys.path.append("./")
+from datetime import datetime
+
+
+# Third-party library imports
+import cv2 as cv
+import numpy as np
+from PIL import Image
+import pandas as pd
+import pickle
+import dlib
+from pathlib import Path
+import face_recognition
+
+# Framework imports
+from flask import request, render_template, flash
+
+# Deep learning and computer vision imports
+from cbir import ImageProcessor
+from scipy.spatial import distance
+
+
+database = Path("app/static/database")
+faces_directory = database / "faces"
+
+
+
+# Read image features
+fe = ImageProcessor()
+features = []
+img_paths = []
+for feature_path in path.glob("*.npy"):
+    features.append(np.load(feature_path))
+    img_paths.append(path.parts[-1] + "/"+ (feature_path.stem + ".png"))
+features = np.array(features, dtype=object).astype(float)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Access the app context using the app object
-    # Your route logic here
-    return 'Hello World'
+    if request.method == 'POST':
+        file = request.files['query_img']  # Save query image
+        try:
+            img = Image.open(file.stream)  # PIL image
+        except:
+            raise Exception(
+                flash("Can't Open file. Insert another file.", "danger"))
+        uploaded_img_path = "ycbir/static/uploaded/" + datetime.now().isoformat().replace(":",
+                                                                                          ".") + "_" + file.filename
+        img.save(uploaded_img_path)
+
+        # Run search
+        query = fe.extract(img).astype(float)
+        # L2 distances to features
+        # dists = np.linalg.norm(features-query, axis=1)
+        dists = list(map(
+            lambda x: 1 - distance.cosine(x, query), features))
+        ids = np.argsort(dists)[:30]  # Top 30 results
+        scores = [(dists[id], img_paths[id]) for id in ids]
+        base_path = Path("ycbir/static")
+
+        return render_template('index.html',
+                               query_path=Path(uploaded_img_path).relative_to(base_path),
+                               scores=scores)
+    else:
+        return render_template('index.html')
+    
