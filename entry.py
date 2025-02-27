59cf94b972136fc4f3d1c4fd97ab1b8f9579a8e8
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2025-02-27 22:00:58 +0100

    use processes instead of threads for extracting faces

--- entry.py
+++ entry.py
@@ -1,7 +1,7 @@
 import subprocess
 import time
 import os
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, wait
 from pathlib import Path
 from app.cbir import ImageProcessor
 import threading
@@ -17,13 +17,12 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 def convert_all_faces_to_embeddings():
     """Use multiprocessing to run face extraction in parallel."""
     with ProcessPoolExecutor(max_workers=4) as executor:  # Adjust workers based on CPU cores
-        executor.map(fe.extract_faces_process, img_repo_list)
-
-def start_face_extraction():
-    """Runs face extraction in a background thread to avoid blocking Flask."""
-    thread = threading.Thread(target=convert_all_faces_to_embeddings, daemon=True)
-    thread.start()
-
+        subprocess.run(["flask", "run"])
+        # executor.map(fe.extract_faces_process, img_repo_list)  
+        for _ in executor.map(fe.extract_faces_process, img_repo_list):
+            pass
+        
 if __name__ == '__main__':
-    start_face_extraction()
-    subprocess.run(["flask", "run"])
+    convert_all_faces_to_embeddings()
+    # executor.submit(subprocess.run(["flask", "run"]))
+    
