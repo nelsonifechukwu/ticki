b24a8f845174834b8c2cf44d2b4f7fd2b1baa1fe
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2025-03-08 07:25:11 +0100

    modified: changed path from PathPosix to os paths

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from config import Config
 from pathlib import Path
+import os
 from .tasks import extract_faces_batch
 
-database = Path("app/static/database")
-img_repo = database / "img-rep"
-img_repo_list = list(img_repo.iterdir())
-img_repo_list = [str(img) for img in img_repo_list]
+# database = Path(app.root_path) / "static" / "database"
+# img_repo = database / "img_repo"
+# img_repo_list = [str(img) for img in img_repo.iterdir()]
+
+# Get the absolute directory of the current script (this file)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Construct absolute path to the database/img_repo directory
+database = os.path.join(basedir, "static", "database")
+img_repo = os.path.join(database, "img_repo")
+
+# List image files
+img_repo_list = [os.path.join(img_repo, img) for img in os.listdir(img_repo)]
 
 def create_app(config_class=Config):
     app = Flask(__name__)
