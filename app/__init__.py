95de917b76d4b994b653adda61c1b2e1e5e079ca
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2025-03-17 10:22:37 +0100

    cleaned code

--- app/__init__.py
+++ app/__init__.py
@@ -5,18 +5,7 @@ import os
 import redis
 redis_client = redis.Redis(host='localhost', port=6379, db=1)
 __all__ = ["redis_client"]
-from .tasks import extract_faces_batch
-
-# Get the absolute directory of the current script (this file)
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# Construct absolute path to the database/img_repo directory
-database = os.path.join(basedir, "static", "database")
-img_repo = os.path.join(database, "img_repo")
-allowed_exts=("jpg", "png", "jpeg")
-
-# List image files
-img_repo_list = [os.path.join(img_repo, img) for img in os.listdir(img_repo) if img.endswith(allowed_exts)]
+from .tasks import extract_all_faces
 
 def create_app(config_class=Config, repeat_tasks=False):
     app = Flask(__name__)
@@ -24,7 +13,7 @@ def create_app(config_class=Config, repeat_tasks=False):
     if repeat_tasks:
         redis_client.flushdb()
     with app.app_context():
-        extract_faces_batch.delay(img_repo_list, repeat_tasks)  # Celery will handle concurrency efficiently
+        extract_all_faces(repeat_tasks) # Uses Celery to handle concurrency efficiently
     return app
 
 app = create_app(repeat_tasks=False)
