111abf027fe8d7111144a662452bc667130e8598
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2025-03-23 06:27:05 +0100

    cleaned app factory function

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,16 @@ from flask import Flask
 from config import Config
 from pathlib import Path
 import os
-from .tasks import extract_all_faces, redis_client
+# from .tasks import extract_all_faces, redis_client
 
 def create_app(config_class=Config, repeat_tasks=False):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    if repeat_tasks:
-        redis_client.flushdb()
-    
-    with app.app_context():
-        extract_all_faces(repeat_tasks) # Uses Celery to handle concurrency efficiently
+    # if repeat_tasks:
+    #     redis_client.flushdb()
+    # with app.app_context():
+    #     extract_all_faces(repeat_tasks) # Uses Celery to handle concurrency efficiently
     return app
 
-app = create_app(repeat_tasks=True)
+app = create_app()
 from app import routes
