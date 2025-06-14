7e8d675f205507895f60c7b141e7ac9262e2b8f4
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2025-06-14 21:16:51 +0100

    bugFix: Path and str conflict

--- app/cbir.py
+++ app/cbir.py
@@ -190,11 +190,9 @@ class ImageProcessor:
         base_path = Path("app/static")
         for feature_path in self.extracted_faces_embeddings_path.glob("*.npy"):
             features.append(np.load(feature_path))
-            intermediate = feature_path.stem# Gives 'IMG_3011_face_0.JPG'
-
-            # Now get the extension
-            img_ext = intermediate.suffix  # '.JPG'
-            img_name = intermediate.stem.split("_face")[0]  # 'IMG_3011'
+            # From 'IMG_3011_face_0.JPG.npy' to
+            img_ext = feature_path.stem.suffix  # '.JPG'
+            img_name = str(feature_path).split("_face")[0]  # 'IMG_3011'
             
             img_paths.append(self.img_data.relative_to(base_path) / (img_name + img_ext)) #get the reference img of the face
 
