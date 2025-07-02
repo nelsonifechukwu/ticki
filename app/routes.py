9eb4710e44e5db079c465e9f28b39da8b654c831
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2025-07-02 19:50:46 +0100

    feat: modified routes to deal with an input img with multiple faces

--- app/routes.py
+++ app/routes.py
@@ -19,13 +19,19 @@ def inject_paths():
     base_path = Path("app/static") 
 
     img_data_path = fe.img_data.relative_to(base_path)
-    extracted_faces_path = fe.extracted_faces_path.relative_to(base_path)
+    extracted_faces_path = fe.extracted_faces_path.relative_to(base_path) 
 
     return {
         "img_data_path": str(img_data_path) + "/",
         "extracted_faces_path": str(extracted_faces_path) + "/"
-    }
-class HomeResource(Resource): 
+    } 
+
+# e.g. in your app/__init__.py or filters.py
+@app.template_filter('debug') 
+def debug_filter(value):
+    print(f"[JINJA DEBUG] face_name: {value}")
+    return ''  # Don't render anything in HTML
+class HomeResource(Resource):      
     def get(self):
         return make_response(render_template("main.html"), 200)
 
@@ -33,25 +39,26 @@ class HomeResource(Resource):
         threshold = 0.67
         img_stream = request.files.get("query-img")
         _, query_img_path = fe.save_query_image(img_stream)
-        query_img_path_str = str(query_img_path)
+        query_img_path_str = str(query_img_path) 
         
         query_face_paths_str = fe.extract_faces(query_img_path)
         query_face_paths = ast.literal_eval(query_face_paths_str)
         
-        if len(query_face_paths)>1:
+        context = {
+            "input_faces": [],
+            "similarity_info": []
+        }
+        if len(query_face_paths) > 1:
             query_faces_names = [Path(face_path).name for face_path in query_face_paths]
-            return make_response(render_template("main.html", input_faces=query_faces_names), 200)
-        #this will be used in case of multiple faces during an upload. Select only one img for now
-        #store also the embeddings of the multiple faces marked as processed
-        query_face_path = Path(query_face_paths[-1])
+            context["input_faces"] = query_faces_names
+        else:
+            query_face_path = Path(query_face_paths[0])
+            query_feature = fe.extract_features(query_face_path).astype(float)
+            results = self._get_similar_faces(query_feature, threshold)
+            embeddings_handler.mark_as_processed(query_feature, query_img_path_str, query_face_paths)
+            context["similarity_info"] = results
 
-        query_feature = fe.extract_features(query_face_path).astype(float)
-
-        results = self._get_similar_faces(query_feature, threshold)
-        ###################
-        embeddings_handler.mark_as_processed(query_feature, query_img_path_str, query_face_paths)
-        ###################
-        return make_response(render_template("main.html", file_info=results), 200)
+        return make_response(render_template("main.html", **context), 200) 
 
     def _get_similar_faces(self, query_feature: np.ndarray, threshold: float):
         # if not all_face_embeddings:
