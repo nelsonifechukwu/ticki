c8241b2d99ccd239cf43aa403acc55c7f910da28
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2025-08-30 08:47:08 +0100

    bugFix: don't expose the cloudinary app to the whole network

--- cloudinary_wb.py
+++ cloudinary_wb.py
@@ -167,4 +167,4 @@ api.add_resource(CloudinaryWebhook, "/cloudinary-webhook")
 api.add_resource(CloudinaryUpload, "/upload")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
