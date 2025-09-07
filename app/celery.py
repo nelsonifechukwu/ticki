79460218102c6588dc06733f337aecf61db02770
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2025-09-07 20:57:04 +0100

    feat: deduplicate tasks

--- app/celery.py
+++ app/celery.py
@@ -4,7 +4,9 @@ from celery import Celery, group, chord
 celery_app = Celery('app', broker='redis://localhost:6379/0', include=['app.tasks'], backend='redis://localhost:6379/0')
 redis_client = redis.Redis(host='localhost', port=6379, db=1) 	
 
+# Configure task deduplication
+celery_app.conf.worker_deduplicate_successful_tasks = True
+celery_app.conf.task_acks_late = True
+celery_app.conf.worker_prefetch_multiplier = 1
+
 __all__ = ['redis', 'celery_app', 'redis_client', 'group', 'chord']
-#celery_app.conf.broker_transport_options = {'visibility_timeout': 9999999}
-#celery_app.conf.worker_deduplicate_successful_tasks = True
-#celery_app.conf.task_acks_late=True
